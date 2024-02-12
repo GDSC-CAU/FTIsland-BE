48d898533d06bc88355584dd213df013ad6a14f8
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-12 23:14:51 +0900

    [fix] user, role entity 필드 수정
    - User의 경우 ERD에 나온 내용 그대로 반영하도록 필드 수정
    - Role의 경우 admin, guest, user 세 필드가 존재하도록 수정 + title 없이 key만으로 구분 가능하므로 title 삭제
    - spring security에서는 권한(ROLE) 코드에 항상 접두사 "ROLE_"이 앞에 붙어야 함
    
    #15

--- BE/src/main/java/com/FTIsland/BE/entity/User.java
+++ BE/src/main/java/com/FTIsland/BE/entity/User.java
@@ -2,41 +2,63 @@ package com.FTIsland.BE.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Entity
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
 @Table(name = "user")
+@AllArgsConstructor
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @NotNull
     private String name;
-
-    @NotNull
     private String email;
-
-    @Column
     private String picture;
+    private boolean isParent;
 
-    @Enumerated(EnumType.STRING) // Enum 타입은 문자열 형태로 저장해야 함
-    @NotNull
+    // 이후 OAuth2 로그인 성공 시 추가 정보를 입력하는 폼으로 이동하도록 구현
+    private int level;
+    private String mainLanguage;
+    private String subLanguage;
+
+    // Enum 타입은 문자열 형태로 저장해야 함
+    // 첫 로그인 시에 Role을 Guest로 설정, 추가 정보 입력 후 User로 업데이트
+    @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Builder
-    public User(String name, String email, String picture, Role role) {
-        this.name = name;
-        this.email = email;
-        this.picture = picture;
-        this.role = role;
+    // JWT RefreshToken 필드
+    // JWT를 이용해 로그인 성공 시 AccessToken, RefreshToken을 발행하는데
+    // 발행된 RefreshToken을 User entity에 저장
+    private String refreshToken;
+
+    // user 권한 설정
+    public void authorizeUser() {
+        this.role = Role.USER;
     }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+
+//    @Builder
+//    public User(String name, String email, String picture, Role role,
+//                boolean isParent, int level, String mainLanguage, String subLanguage) {
+//        this.name = name;
+//        this.email = email;
+//        this.picture = picture;
+//        this.role = role;
+//        this.isParent = isParent;
+//        this.level = level;
+//        this.mainLanguage = mainLanguage;
+//        this.subLanguage = subLanguage;
+//    }
 
     public User update(String name, String picture) {
         this.name = name;
