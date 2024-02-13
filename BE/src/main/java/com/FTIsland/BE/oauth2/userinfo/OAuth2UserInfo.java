af01e507746bc57d92129f633d9722c961a97841
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-13 22:09:17 +0900

    [fix] 하나의 구글 이메일 계정으로 아이, 부모 회원 가입을 위한 OAuth2UserInfo 수정
    - 이메일, 부모 여부(isParent) 담도록 수정
    
    #15

--- BE/src/main/java/com/FTIsland/BE/oauth2/userinfo/OAuth2UserInfo.java
+++ BE/src/main/java/com/FTIsland/BE/oauth2/userinfo/OAuth2UserInfo.java
@@ -15,4 +15,6 @@ public abstract class OAuth2UserInfo {
     public abstract String getName();
 
     public abstract String getImageUrl();
+    public abstract String getEmail();
+    public abstract boolean getisParent();
 }
