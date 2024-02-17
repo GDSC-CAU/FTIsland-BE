f7366bd71ad8411f9408357425117dd47f20a9bc
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-17 20:55:53 +0900

    [fix] authentication 인증 정보에서 id 추출 및 id로 jwt accesstoken 만들기

--- BE/src/main/java/com/FTIsland/BE/login/handler/LoginSuccessHandler.java
+++ BE/src/main/java/com/FTIsland/BE/login/handler/LoginSuccessHandler.java
@@ -24,8 +24,10 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
-        String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
-        String accessToken = jwtService.createAccessToken(email); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
+        String email = extractUsername(authentication); // 인증 정보에서 email 추출
+        Long id = extractUserId(authentication); // 인증 정보에서 id 추출
+
+        String accessToken = jwtService.createAccessToken(id, email); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
@@ -35,6 +37,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
                     user.updateRefreshToken(refreshToken);
                     userRepository.saveAndFlush(user);
                 });
+        log.info("로그인에 성공하였습니다. id : {}", id);
         log.info("로그인에 성공하였습니다. 이메일 : {}", email);
         log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
         log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
@@ -43,5 +46,12 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private String extractUsername(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return userDetails.getUsername();
+    }
+
+    // extractUserId 임시 생성
+    private Long extractUserId(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Long id = 123456L; // 임시로 id 생성, 원래는 email로 등록한 후 return된 id를 받아야 함
+        return id;
     }
 }
