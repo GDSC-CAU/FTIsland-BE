f3d05631267ba2e98bfa64cf0e8bb11822ab491e
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-17 14:29:47 +0900

    [feat] /language를 다루는 LanguageController 생성
    - @RequestBody로 UserLanguageDTO를 전달
    - 저장 후 userService에서 넘어온 userLanguageDTO를 그대로 return

---
+++ BE/src/main/java/com/FTIsland/BE/controller/LanguageController.java
@@ -0,0 +1,23 @@
+package com.FTIsland.BE.controller;
+
+import com.FTIsland.BE.dto.UserLanguageDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.FTIsland.BE.service.UserService;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class LanguageController {
+
+    private final UserService userService;
+
+    @PostMapping("/language")
+    public UserLanguageDTO saveUserLanguage(@RequestBody UserLanguageDTO userLanguageDTO){ // DB에 동화 정보 저장
+        return userService.save(userLanguageDTO);
+    }
+}
