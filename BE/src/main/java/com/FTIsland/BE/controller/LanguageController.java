94ed931d824e5825d91177d6654ae78cb9c197c9
alsrudrl1220 <alsrudrl1220@naver.com>
2024-02-21 03:52:56 +0900

    [fix] Post -> Put으로 Mapping annotation 수정

--- BE/src/main/java/com/FTIsland/BE/controller/LanguageController.java
+++ BE/src/main/java/com/FTIsland/BE/controller/LanguageController.java
@@ -3,10 +3,7 @@ package com.FTIsland.BE.controller;
 import com.FTIsland.BE.dto.UserLanguageDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.FTIsland.BE.service.UserService;
 
 @Slf4j
@@ -16,7 +13,7 @@ public class LanguageController {
 
     private final UserService userService;
 
-    @PostMapping("/language")
+    @PutMapping("/language")
     public UserLanguageDTO saveUserLanguage(@RequestBody UserLanguageDTO userLanguageDTO){
         return userService.save(userLanguageDTO);
     }
