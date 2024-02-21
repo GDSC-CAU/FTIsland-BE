31d6ef08d22ddb63e42c087bc4adcb98aea402c6
alsrudrl1220 <alsrudrl1220@naver.com>
2024-02-21 09:44:28 +0900

    [feat] description으로 조회 + 저장 관련 가이드라인 작성

--- BE/src/main/java/com/FTIsland/BE/controller/QuizController.java
+++ BE/src/main/java/com/FTIsland/BE/controller/QuizController.java
@@ -32,11 +32,24 @@ public class QuizController {
         // 책 이름으로 생각해보기 질문을 생성해야하기 때문에 bookId를 통해 동화 제목 조회
         String bookTitle = bookInfoService.findNameById(quizDTO.getBookId());
 
-        // 퀴즈 생성
+        // 책의 description 조회 - 동화 제목만으로는 버전이 다를 수 있기 때문에
+        // String description = bookInfoService.findDescriptionByUd(quizDTO.getBookId());
+
+        // 이미 해당 동화-레벨 맞춤형 질문이 있으면 DB에서 가져오기 / 없다면 퀴즈 생성 후 저장
+          // 1. quizService에서 동화-레벨 쌍으로 검색
+
+          // 2-a. 있으면
+
+          // 2-b. 없다면
+
+            // 1. 퀴즈 생성
         ChatGptResponse chatGptResponse = null;
         chatGptResponse = chatGptService.askQuestion(bookTitle, userLevel);
         String threeQuiz = chatGptResponse.getChoices().get(0).getMessage().getContent();
         System.out.println(threeQuiz);
+
+            // 2. 저장
+
 
         // 생성된 퀴즈 3개를 파싱하고 해당 퀴즈 리스트를 주언어, 서브언어로 번역 후 반환
         return quizService.translationQuiz(
