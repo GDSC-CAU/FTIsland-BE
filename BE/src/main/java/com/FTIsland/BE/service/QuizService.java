73433a920b277c879b74d17c4edf6f7848402771
alsrudrl1220 <alsrudrl1220@naver.com>
2024-02-21 23:50:29 +0900

    [fix] translationQuiz method도 DTO List에서 String List로 바뀐 부분 반영

--- BE/src/main/java/com/FTIsland/BE/service/QuizService.java
+++ BE/src/main/java/com/FTIsland/BE/service/QuizService.java
@@ -34,18 +34,18 @@ public class QuizService {
     }
 
     // 3개의 퀴즈 list를 번역 후 QuizDTO로 반환
-    public List<QuizDTO> translationQuiz(List<QuizDTO> quizDTOS, String mainLan, String subLan) {
+    public List<QuizDTO> translationQuiz(List<String> quizs, String mainLan, String subLan) {
 
         List<QuizDTO> translatedQuizDTOS = new ArrayList<>();
 
         String selectedLanguage = "ko";
 
-        for (QuizDTO dto : quizDTOS) {
+        for (String quiz : quizs) {
             // 번역 로직
 
             // 1. 주, 보조 언어 텍스트 둘 다 한국어를 기본으로 설정
-            String mainText = dto.getQuestion();
-            String subText = dto.getQuestion();
+            String mainText = quiz;
+            String subText = quiz;
 
             // 2. 요청한 언어에 맞게 번역
             if (mainLan.equals("ko")) { // 주 언어가 한국어가 아니라면 번역해서 저장
