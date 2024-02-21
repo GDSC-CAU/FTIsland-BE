ab14f4492ecd346474e4e8117e82b4ec9ebb50ab
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-21 21:33:05 +0900

    [fix] 회원 가입 시 비밀번호 유효성 검사 기준 완화
    - 기존 영문 대/소문자, 숫자, 특수문자 포함 8-16자에서 8자 이상 16자 이하로 변경

--- BE/src/main/java/com/FTIsland/BE/dto/SignUpDTO.java
+++ BE/src/main/java/com/FTIsland/BE/dto/SignUpDTO.java
@@ -19,7 +19,7 @@ public class SignUpDTO {
     private String inputId;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 영문 대/소문자, 숫자, 특수 문자로 이루어진 8~16자여야 합니다.")
+    @Pattern(regexp = "^.{8,16}$", message = "비밀번호는 8자 이상 16자 이하이어야 합니다.")
     private String inputPassword;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
