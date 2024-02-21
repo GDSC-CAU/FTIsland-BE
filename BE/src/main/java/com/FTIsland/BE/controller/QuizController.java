bb40aa384937544bdbdc4dca461e7e4a5b2c3e53
alsrudrl1220 <alsrudrl1220@naver.com>
2024-02-22 00:12:50 +0900

    [feat] 이미 저장된 퀴즈가 있으면 번역 후 반환 없으면 생성하고 저장하여 번역 후 반환하도록 기능 구현

--- BE/src/main/java/com/FTIsland/BE/controller/QuizController.java
+++ BE/src/main/java/com/FTIsland/BE/controller/QuizController.java
@@ -33,41 +33,53 @@ public class QuizController {
         String bookTitle = bookInfoService.findNameById(quizDTO.getBookId());
 
         // 책 - 사용자 레벨 쌍의 조합으로 저장되어있는 생각해보기 질문이 있는지 확인
+        List<String> savedQuiz = quizService.findQuiz(quizDTO.getBookId(), userLevel);
 
+        if(savedQuiz.isEmpty()){ // 없으면 새로 생성 후 저장
+            // 책의 contents 조회 - 동화 제목만으로는 버전이 다를 수 있기 때문에
+            // 1. bookContentService의 findByBookId method를 쓰기 위해 QuizDTO -> BookContentDTO 변환
+            BookContentDTO bookContentDTO = new BookContentDTO();
+            bookContentDTO.setBookId(quizDTO.getBookId());
+            bookContentDTO.setMainLan(quizDTO.getMainLan());
+            bookContentDTO.setSubLan(quizDTO.getSubLan());
+            // 2. content List 조회
+            List<BookContentDTO> bookContentDTOS = bookContentService.findByBookId(bookContentDTO);
+            List<String> bookContentList = new ArrayList<>();
+            for(BookContentDTO dto : bookContentDTOS){
+                bookContentList.add(dto.getKorContents());
+            }
+            String bookContents = String.join(" ", bookContentList);
+            // System.out.println(bookContents);
 
-        // 있으면 조회한 질문을 그대로 번역 후 반환
+            // 퀴즈 생성
+            ChatGptResponse chatGptResponse = null;
+            chatGptResponse = chatGptService.askQuestion(bookTitle, userLevel, bookContents);
+            String threeQuiz = chatGptResponse.getChoices().get(0).getMessage().getContent();
+            // System.out.println(threeQuiz);
 
-        // 없으면 새로 생성 후 저장
+            // 생성된 퀴즈 3개를 파싱하고 저장
+            List<String> quizs = quizService.makeQuiz(threeQuiz);
+            quizService.saveQuiz(quizs, quizDTO.getBookId(), userLevel);
 
-        // 책의 contents 조회 - 동화 제목만으로는 버전이 다를 수 있기 때문에
-        // 1. bookContentService의 findByBookId method를 쓰기 위해 QuizDTO -> BookContentDTO 변환
-        BookContentDTO bookContentDTO = new BookContentDTO();
-        bookContentDTO.setBookId(quizDTO.getBookId());
-        bookContentDTO.setMainLan(quizDTO.getMainLan());
-        bookContentDTO.setSubLan(quizDTO.getSubLan());
-        // 2. content List 조회
-        List<BookContentDTO> bookContentDTOS = bookContentService.findByBookId(bookContentDTO);
-        List<String> bookContentList = new ArrayList<>();
-        for(BookContentDTO dto : bookContentDTOS){
-            bookContentList.add(dto.getKorContents());
+            // 해당 퀴즈 리스트를 주언어, 서브언어로 번역 후 반환
+            return quizService.translationQuiz(
+                    quizs,
+                    quizDTO.getMainLan(),
+                    quizDTO.getSubLan()
+            );
+
+        } else { // 있으면 조회한 질문을 그대로 번역 후 반환
+            return quizService.translationQuiz(
+                    savedQuiz,
+                    quizDTO.getMainLan(),
+                    quizDTO.getSubLan()
+            );
         }
-        String bookContents = String.join(" ", bookContentList);
-        // System.out.println(bookContents);
-
-        // 퀴즈 생성
-        ChatGptResponse chatGptResponse = null;
-        chatGptResponse = chatGptService.askQuestion(bookTitle, userLevel, bookContents);
-        String threeQuiz = chatGptResponse.getChoices().get(0).getMessage().getContent();
-        // System.out.println(threeQuiz);
-
-        // 생성된 퀴즈 3개를 파싱하고 저장
-        List<String> quizs = quizService.makeQuiz(threeQuiz);
-        quizService.saveQuiz(quizs, quizDTO.getBookId(), userLevel);
-
-        // 해당 퀴즈 리스트를 주언어, 서브언어로 번역 후 반환
-        return quizService.translationQuiz(
-                quizs,
-                quizDTO.getMainLan(),
-                quizDTO.getSubLan());
     }
+
+
+
+
+
+
 }
