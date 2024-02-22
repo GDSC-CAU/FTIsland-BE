9c5234bdf3b4eff637069b8445f8dc62a26ed2b9
alsrudrl1220 <alsrudrl1220@naver.com>
2024-02-22 21:43:04 +0900

    [feat] BookVocaDescriptionDTO 추가

---
+++ BE/src/main/java/com/FTIsland/BE/dto/BookVocaDescriptionDTO.java
@@ -0,0 +1,16 @@
+package com.FTIsland.BE.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class BookVocaDescriptionDTO {
+    private String description;
+    private String image;
+
+}
