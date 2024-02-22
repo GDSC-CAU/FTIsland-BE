842de2cfffe08f4625d2533f4af217a844979398
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-22 22:02:59 +0900

    [fix] read repository에서 findByUserIdAndBookId를 @query를 사용하도록 수정

--- BE/src/main/java/com/FTIsland/BE/repository/ReadRepository.java
+++ BE/src/main/java/com/FTIsland/BE/repository/ReadRepository.java
@@ -2,6 +2,7 @@ package com.FTIsland.BE.repository;
 
 import com.FTIsland.BE.entity.ReadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface ReadRepository extends JpaRepository<ReadEntity, Integer> {
+
+    @Query("SELECT r FROM ReadEntity r WHERE r.userId = :userId AND r.bookId = :bookId")
     Optional<ReadEntity> findByUserIdAndBookId(Integer userId, Integer bookId);
     List<ReadEntity> findByUserId(Integer userId);
     List<ReadEntity> findAllByUserIdOrderByUpdatedAtDesc(Integer userId);
