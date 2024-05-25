217306b6089dece2c867c10533f6b2570541b4d6
rumos <gyunjin3@gmail.com>
2024-05-25 16:35:22 +0900

    [feat] IslandInfoEntity 빌더 생성자

--- BE/src/main/java/com/FTIsland/BE/entity/IslandInfoEntity.java
+++ BE/src/main/java/com/FTIsland/BE/entity/IslandInfoEntity.java
@@ -2,13 +2,16 @@ package com.FTIsland.BE.entity;
 
 import com.FTIsland.BE.dto.IslandInfoDTO;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
 @Table(name = "islandinfo")
+@NoArgsConstructor
 public class IslandInfoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +25,11 @@ public class IslandInfoEntity {
         islandInfoEntity.setId(islandInfoDTO.getId());
         islandInfoEntity.setName(islandInfoDTO.getName());
         return islandInfoEntity;
+    }
+
+    @Builder
+    public IslandInfoEntity(Integer id, String name) {
+        this.id = id;
+        this.name = name;
     }
 }
