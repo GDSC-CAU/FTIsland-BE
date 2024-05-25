b18291c4e10458dbf3654ca9ae10f061c5ff5c6d
rumos <gyunjin3@gmail.com>
2024-05-25 23:26:17 +0900

    [fix] 기존 island info dto를 island info response로 변경

---
+++ BE/src/main/java/com/FTIsland/BE/island/dto/IslandInfoResponse.java
@@ -0,0 +1,23 @@
+package com.FTIsland.BE.island.dto;
+
+import com.FTIsland.BE.island.entity.IslandInfoEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class IslandInfoResponse {
+    private Integer id;
+    private String name;
+
+    @Builder
+    public IslandInfoResponse(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public IslandInfoEntity toEntity() {
+        return IslandInfoEntity.builder().id(id).name(name).build();
+    }
+}
