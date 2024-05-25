973a854e747f4a4cd4943eb89b637020577dec21
rumos <gyunjin3@gmail.com>
2024-05-25 16:43:42 +0900

    [fix] seed data 추가로 인한 save 메서드 삭제

--- BE/src/main/java/com/FTIsland/BE/service/IslandInfoService.java
+++ BE/src/main/java/com/FTIsland/BE/service/IslandInfoService.java
@@ -13,23 +13,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class IslandInfoService {
     private final IslandInfoRepository islandInfoRepository;
-
-    public void save() { // 동화 정보 저장(임시로 만든 method)
-        ArrayList<IslandInfoDTO> islandInfoDTOS = new ArrayList<>();
-        islandInfoDTOS.add(new IslandInfoDTO(1, "지혜"));
-        islandInfoDTOS.add(new IslandInfoDTO(2, "기쁨"));
-        islandInfoDTOS.add(new IslandInfoDTO(3, "행복"));
-        islandInfoDTOS.add(new IslandInfoDTO(4, "용기"));
-        islandInfoDTOS.add(new IslandInfoDTO(5, "희망"));
-        islandInfoDTOS.add(new IslandInfoDTO(6, "미지"));
-
-        for(IslandInfoDTO islandInfoDTO : islandInfoDTOS) {
-            IslandInfoEntity islandInfoEntity = IslandInfoEntity.toIslandInfoEntity(islandInfoDTO);
-            islandInfoRepository.save(islandInfoEntity);
-        }
-
-    }
-
+    
     public IslandInfoDTO findById(Integer islandId) {
         Optional<IslandInfoEntity> islandInfoEntity = islandInfoRepository.findById(islandId);
         if(islandInfoEntity.isPresent()) {
