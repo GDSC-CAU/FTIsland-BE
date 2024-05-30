e35580a55d3461c786dbb77f51aea72a8d86232e
rumos <gyunjin3@gmail.com>
2024-05-30 22:38:33 +0900

    [feat] IslandBooksDTO에서 IslandBooksRequest로 변경, Builder 적용

---
+++ BE/src/main/java/com/FTIsland/BE/island/dto/IslandBooksRequest.java
@@ -0,0 +1,38 @@
+package com.FTIsland.BE.island.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class IslandBooksRequest {
+    private Integer userId;
+    private Integer bookId;
+    private Integer islandId;
+    private String title;
+    private String description;
+    private String category;
+    private String country;
+    private Integer totalPage;
+    private String image;
+    private float progress;
+
+    @Builder
+    public IslandBooksRequest(Integer userId, Integer bookId, Integer islandId, String title, String description,
+                          String category, String country, Integer totalPage, String image, float progress) {
+        this.userId = userId;
+        this.bookId = bookId;
+        this.islandId = islandId;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.country = country;
+        this.totalPage = totalPage;
+        this.image = image;
+        this.progress = progress;
+    }
+}
+
