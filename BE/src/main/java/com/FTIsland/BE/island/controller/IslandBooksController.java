a6a40d701c05cbcccb75b03acce0188d9cf94530
rumos <gyunjin3@gmail.com>
2024-06-06 13:31:17 +0900

    [refactor] service에서 받은 정보를 for문을 통해 변환하지 않고, stream과 map을 이용해 response DTO로 변환

--- BE/src/main/java/com/FTIsland/BE/island/controller/IslandBooksController.java
+++ BE/src/main/java/com/FTIsland/BE/island/controller/IslandBooksController.java
@@ -1,8 +1,9 @@
 package com.FTIsland.BE.island.controller;
 
 import com.FTIsland.BE.dto.IslandBooksDTO;
-import com.FTIsland.BE.island.entity.BookInfoEntity;
 import com.FTIsland.BE.island.dto.IslandBooksRequest;
+import com.FTIsland.BE.island.entity.BookInfoEntity;
+import com.FTIsland.BE.island.dto.IslandBooksResponse;
 import com.FTIsland.BE.service.BookInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -22,33 +23,24 @@ public class IslandBooksController {
     private final BookInfoService bookInfoService;
 
     @GetMapping("/island/books")
-    public List<IslandBooksDTO> getBookInfoByIslandId(@RequestBody IslandBooksRequest islandBooksRequest) {
+    public List<IslandBooksResponse> getBookInfoByIslandId(@RequestBody IslandBooksRequest islandBooksRequest) {
         // userId가 -1이라면 비회원이므로 book info db를 island id로 검색한 결과만 리턴
         // 비회원인 경우 반환 정상
-        List<IslandBooksDTO> islandBooksDTOList = new ArrayList<>();
-        // 섬 id를 이용해서 섬에 속한 책 정보를 가져옴
-        // 섬 : 책 = 일 : 다 릴레이션 필요
-        List<Optional<BookInfoEntity>> bookInfoEntityList = bookInfoService.findNameByIslandId(islandBooksRequest.getIslandId());
 
-        // IslandBooksDTO로 변환해서 list로 리턴
-        for (Optional<BookInfoEntity> bookInfoEntityOptional : bookInfoEntityList) {
-            bookInfoEntityOptional.ifPresent(bookInfoEntity -> {
-                IslandBooksDTO dto = new IslandBooksDTO(
-                        islandBooksRequest.getUserId(), // 비회원일 경우 userId는 -1로 설정
-                        bookInfoEntity.getId(),
-                        islandBooksRequest.getIslandId(),
-                        bookInfoEntity.getTitle(),
-                        bookInfoEntity.getDescription(),
-                        bookInfoEntity.getCategory(),
-                        bookInfoEntity.getCountry(),
-                        bookInfoEntity.getTotalPage(),
-                        bookInfoEntity.getImage(),
-                        0.00f
-                );
+        List<BookInfoEntity> bookInfoEntityList = bookInfoService.findBookInfoByIslandId(islandBooksRequest.getIslandId());
+        List<IslandBooksResponse> collect = bookInfoEntityList
+                .stream()
+                .map(m -> new IslandBooksResponse().builder()
+                        .bookId(m.getId())
+                        .title(m.getTitle())
+                        .description(m.getDescription())
+                        .category(m.getCategory())
+                        .country(m.getCountry())
+                        .totalPage(m.getTotalPage())
+                        .image(m.getImage())
+                        .build())
+                .toList();
 
-                islandBooksDTOList.add(dto);
-            });
-        }
-        return islandBooksDTOList;
+        return collect;
     }
 }
