d8b9d54da87451f6c87567d1f5a8527eabf89c09
rumos <gyunjin3@gmail.com>
2024-06-06 13:55:26 +0900

    [fix] refactor에 따른 import 정리

--- BE/src/main/java/com/FTIsland/BE/service/BookInfoService.java
+++ BE/src/main/java/com/FTIsland/BE/service/BookInfoService.java
@@ -5,11 +5,8 @@ import com.FTIsland.BE.island.entity.BookInfoEntity;
 import com.FTIsland.BE.repository.BookInfoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
