ce32a31b41f716945b1b9f6a64d8bfcf3c9e1497
rumos <gyunjin3@gmail.com>
2024-06-06 13:54:25 +0900

    [refactor] island books > findBookInfoByIslandId 메서드 작성

--- BE/src/main/java/com/FTIsland/BE/island/service/IslandBooksService.java
+++ BE/src/main/java/com/FTIsland/BE/island/service/IslandBooksService.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class IslandBooksService {
 
     private final BookInfoRepository bookInfoRepository;
+
+    @Transactional
+    public List<BookInfoEntity> findBookInfoByIslandId(Integer islandId) {
+        return bookInfoRepository.findByIslandInfoEntityId(islandId);
+    }
 
     @Transactional
     public List<BookInfoEntity> findNameByIslandId(Integer islandId) {
