6eda5efe44dc5e1e8ffce35933121c0dfd4ef9da
rumos <gyunjin3@gmail.com>
2024-06-06 13:54:58 +0900

    [refactor] island books 컨트롤러가 book info 서비스를 이용하는 현상 해결 (메서드를 island books 서비스로 옮김)

--- BE/src/main/java/com/FTIsland/BE/island/controller/IslandBooksController.java
+++ BE/src/main/java/com/FTIsland/BE/island/controller/IslandBooksController.java
@@ -1,31 +1,28 @@
 package com.FTIsland.BE.island.controller;
 
-import com.FTIsland.BE.dto.IslandBooksDTO;
 import com.FTIsland.BE.island.dto.IslandBooksRequest;
 import com.FTIsland.BE.island.entity.BookInfoEntity;
 import com.FTIsland.BE.island.dto.IslandBooksResponse;
-import com.FTIsland.BE.service.BookInfoService;
+import com.FTIsland.BE.island.service.IslandBooksService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class IslandBooksController {
 
-    private final BookInfoService bookInfoService;
+    private final IslandBooksService islandBooksService;
 
     @GetMapping("/island/books")
     public List<IslandBooksResponse> getBookInfoByIslandId(@RequestBody IslandBooksRequest islandBooksRequest) {
 
-        List<BookInfoEntity> bookInfoEntityList = bookInfoService.findBookInfoByIslandId(islandBooksRequest.getIslandId());
+        List<BookInfoEntity> bookInfoEntityList = islandBooksService.findBookInfoByIslandId(islandBooksRequest.getIslandId());
         List<IslandBooksResponse> collect = bookInfoEntityList
                 .stream()
                 .map(m -> new IslandBooksResponse().builder()
