81bbbe01015e4a359c4ad65e846a1c4904ff89c1
rumos <gyunjin3@gmail.com>
2024-06-07 21:38:08 +0900

    [feat] IslandBooksService 서비스 테스트 코드 작성

---
+++ BE/src/test/java/com/FTIsland/BE/island/service/IslandBooksServiceTest.java
@@ -0,0 +1,34 @@
+package com.FTIsland.BE.island.service;
+
+import com.FTIsland.BE.island.entity.BookInfoEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class IslandBooksServiceTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private IslandBooksService islandBooksService;
+
+    @Test
+    @DisplayName("섬 동화 정보 불러오기 서비스 테스트")
+    void 섬_동화_정보_불러오기() {
+        // given
+        Integer islandId = 1;
+
+        // when
+        List<BookInfoEntity> bookInfoEntities = islandBooksService.findBookInfoByIslandId(islandId);
+
+        // then
+        assertThat(bookInfoEntities.size()).isEqualTo(4);
+    }
+}
