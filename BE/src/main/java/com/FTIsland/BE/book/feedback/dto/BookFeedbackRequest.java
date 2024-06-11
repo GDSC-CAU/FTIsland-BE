f14407e2098391678306f814490cc63265817338
rumos <gyunjin3@gmail.com>
2024-06-11 23:19:22 +0900

    #93 BookFeedbackDTO -> Request로 변경

--- BE/src/main/java/com/FTIsland/BE/book/feedback/dto/BookFeedbackRequest.java
+++ BE/src/main/java/com/FTIsland/BE/book/feedback/dto/BookFeedbackRequest.java
@@ -1,20 +1,16 @@
 package com.FTIsland.BE.book.feedback.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
-@Setter
 @NoArgsConstructor
-@ToString
-public class BookFeedbackDTO {
+public class BookFeedbackRequest {
 
     private Integer userId;
     private Integer feedback;
 
-    public BookFeedbackDTO(Integer userId, Integer feedback) {
+    @Builder
+    public BookFeedbackRequest(Integer userId, Integer feedback) {
         this.userId = userId;
         this.feedback = feedback;
     }
