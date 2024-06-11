f6a9f15911cad4c71cb40ac886bcd0111e30dc6f
rumos <gyunjin3@gmail.com>
2024-06-11 23:38:23 +0900

    #93 User entity builder 패턴 적용

--- BE/src/main/java/com/FTIsland/BE/entity/User.java
+++ BE/src/main/java/com/FTIsland/BE/entity/User.java
@@ -25,72 +25,19 @@ public class User extends BaseEntity {
     private int level;
     private String mainLanguage;
     private String subLanguage;
-
     private boolean isParent;
 
-    //private String email;
-    //private String password;
-    //private String name;
-    //private String imageUrl; // 프로필 이미지
-
-    //private String emailWithisParent; // email0, email1
-
-    // 이후 OAuth2 로그인 성공 시 추가 정보를 입력하는 폼으로 이동하도록 구현
-
-
-//    // Enum 타입은 문자열 형태로 저장해야 함
-//    // 첫 로그인 시에 Role을 Guest로 설정, 추가 정보 입력 후 User로 업데이트
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
-//
-//    @Enumerated(EnumType.STRING)
-//    private SocialType socialType; // KAKAO, NAVER, GOOGLE
-//
-//    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
-
-
-    // JWT RefreshToken 필드
-    // JWT를 이용해 로그인 성공 시 AccessToken, RefreshToken을 발행하는데
-    // 발행된 RefreshToken을 User entity에 저장
-    //private String refreshToken;
-
-    // user 권한 설정
-//    public void authorizeUser() {
-//        this.role = Role.USER;
-//    }
-//
-//    // 비밀번호 암호화 메소드
-//    public void passwordEncode(PasswordEncoder passwordEncoder) {
-//        this.password = passwordEncoder.encode(this.password);
-//    }
-//
-//    public void updateRefreshToken(String updateRefreshToken) {
-//        this.refreshToken = updateRefreshToken;
-//    }
-
-//    @Builder
-//    public User(String name, String email, String picture, Role role,
-//                boolean isParent, int level, String mainLanguage, String subLanguage) {
-//        this.name = name;
-//        this.email = email;
-//        this.picture = picture;
-//        this.role = role;
-//        this.isParent = isParent;
-//        this.level = level;
-//        this.mainLanguage = mainLanguage;
-//        this.subLanguage = subLanguage;
-//    }
-
-//    public User update(String name, String imageUrl) {
-//        this.name = name;
-//        this.imageUrl = imageUrl;
-//
-//        return this;
-//    }
-//
-//    public String getRoleKey() {
-//        return this.role.getKey();
-//    }
+    @Builder
+    public User(Integer id, String inputId, String inputPassword, String name, int level, String mainLanguage, String subLanguage, boolean isParent) {
+        this.id = id;
+        this.inputId = inputId;
+        this.inputPassword = inputPassword;
+        this.name = name;
+        this.level = level;
+        this.mainLanguage = mainLanguage;
+        this.subLanguage = subLanguage;
+        this.isParent = isParent;
+    }
 
     public static User toUserEntity(SignUpDTO signUpDTO) {
         log.info(signUpDTO.getInputPassword());
