38d03ea6af4f715d754a73829804206d8777b137
rumos <gyunjin3@gmail.com>
2024-06-16 21:08:49 +0900

    #95 IslandInfo 컨트롤러에서 BookProgress 컨트롤러 분리

---
+++ BE/src/main/java/com/FTIsland/BE/book/progress/controller/BookProgressController.java
@@ -0,0 +1,24 @@
+package com.FTIsland.BE.book.progress.controller;
+
+import com.FTIsland.BE.dto.IslandBooksDTO;
+import com.FTIsland.BE.dto.ReadDTO;
+import com.FTIsland.BE.service.ReadService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class BookProgressController {
+    private final ReadService readService;
+
+    @PostMapping("/book/progress")
+    public List<ReadDTO> getBookInfoWithRead(@RequestBody IslandBooksDTO islandBooksDTO) {
+        return readService.progress(islandBooksDTO);
+    }
+}
--- BE/src/main/java/com/FTIsland/BE/island/controller/IslandInfoController.java
+++ BE/src/main/java/com/FTIsland/BE/island/controller/IslandInfoController.java
@@ -1,31 +1,20 @@
 package com.FTIsland.BE.island.controller;
 
-import com.FTIsland.BE.dto.IslandBooksDTO;
-import com.FTIsland.BE.dto.ReadDTO;
 import com.FTIsland.BE.island.dto.IslandInfoRequest;
 import com.FTIsland.BE.island.dto.IslandInfoResponse;
-import com.FTIsland.BE.service.BookInfoService;
 import com.FTIsland.BE.island.service.IslandInfoService;
-import com.FTIsland.BE.service.ReadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class IslandInfoController {
     private final IslandInfoService islandInfoService;
-    private final ReadService readService;
 
     @GetMapping("/island/info")
     public IslandInfoResponse getIslandInfoById(@RequestBody IslandInfoRequest islandInfoRequest) {
         return islandInfoService.findById(islandInfoRequest);
-    }
-
-    @PostMapping("/book/progress")
-    public List<ReadDTO> getBookInfoWithRead(@RequestBody IslandBooksDTO islandBooksDTO) {
-        return readService.progress(islandBooksDTO);
     }
 }
