ba928a5460482f11e60ec0f29e095d703da1d174
rumos <gyunjin3@gmail.com>
2024-06-20 23:41:03 +0900

    #95 Entity > ReadEntity builder 생성자 추가

--- BE/src/main/java/com/FTIsland/BE/entity/ReadEntity.java
+++ BE/src/main/java/com/FTIsland/BE/entity/ReadEntity.java
@@ -3,12 +3,15 @@ package com.FTIsland.BE.entity;
 import com.FTIsland.BE.base.BaseEntity;
 import com.FTIsland.BE.dto.ReadDTO;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@NoArgsConstructor
 @Table(name = "readbook")
 public class ReadEntity extends BaseEntity {
     @Id
@@ -27,6 +30,13 @@ public class ReadEntity extends BaseEntity {
     @Column
     private Integer limitNum;
 
+    @Builder
+    public ReadEntity(Integer userId, Integer bookId, Integer offset_value, Integer limitNum) {
+        this.userId = userId;
+        this.bookId = bookId;
+        this.offset_value = offset_value;
+        this.limitNum = limitNum;
+    }
 
     public static ReadEntity toReadEntity(ReadDTO readDTO){
         ReadEntity readEntity = new ReadEntity();
