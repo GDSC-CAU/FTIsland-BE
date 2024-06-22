633656b2cc9e3cafa4820225a4618c1c486f7d16
rumos <gyunjin3@gmail.com>
2024-06-22 20:14:01 +0900

    #97 ResponseEntity 반환하도록 수정

--- BE/src/main/java/com/FTIsland/BE/user/controller/UserController.java
+++ BE/src/main/java/com/FTIsland/BE/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.FTIsland.BE.user.controller;
 
+import ch.qos.logback.core.status.Status;
+import com.FTIsland.BE.response.ResponseEntity;
+import com.FTIsland.BE.response.StatusCode;
 import com.FTIsland.BE.user.dto.UserLanguageRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.ResponseHandler;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.FTIsland.BE.user.service.UserService;
 
@@ -16,8 +18,9 @@ public class UserController {
     private final UserService userService;
 
     @PatchMapping("/user/language")
-    public ResponseEntity<ResponseHandler<Void>> updateUserLanguage(@RequestBody UserLanguageRequest userLanguageRequest){
+    public ResponseEntity updateUserLanguage(@RequestBody UserLanguageRequest userLanguageRequest){
         userService.patchUserLanguage(userLanguageRequest);
-        return ResponseEntity.ok().build();
+
+        return ResponseEntity.builder().status(StatusCode.OK).data(userLanguageRequest.getUserId()).build();
     }
 }
