d648515a8b2ed5fc5e7f4e84656bf05b22d325ed
rumos <gyunjin3@gmail.com>
2024-06-22 20:01:38 +0900

    #97 /language -> /user/language / Patch 메서드로 변경

---
+++ BE/src/main/java/com/FTIsland/BE/user/controller/UserLanguageController.java
@@ -0,0 +1,23 @@
+package com.FTIsland.BE.user.controller;
+
+import com.FTIsland.BE.user.dto.UserLanguageRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.ResponseHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.FTIsland.BE.user.service.UserService;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class UserLanguageController {
+
+    private final UserService userService;
+
+    @PatchMapping("/user/language")
+    public ResponseEntity<ResponseHandler<Void>> updateUserLanguage(@RequestBody UserLanguageRequest userLanguageRequest){
+        userService.patchUserLanguage(userLanguageRequest);
+        return ResponseEntity.ok().build();
+    }
+}
